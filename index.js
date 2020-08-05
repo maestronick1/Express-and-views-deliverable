7d3f1b6d308242553de067e30abd445b70c63d49
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-08-04 22:02:42 -0700

    finish

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const app = express();
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname+'/views/index.html');
+    res.send('My main page');
 });
 app.get('/about', function(req, res) {
     res.sendFile(__dirname+'/views/about.html');
+    res.send("I am Nick and I am a Software Engineer")
+
 });
 app.get('/blog', function(req, res) {
     res.sendFile(__dirname+'/views/blog-directory.html');
+    res.send("A directory of all my blogs will go here")
 });
 
 app.listen(8000);
